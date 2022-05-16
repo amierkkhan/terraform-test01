e1626f212708b48ea90ba62b0f23c66525387d21
Amir khan <amierkkhan@gmail.com>
2022-05-16 12:29:45 +0530

    06

--- main.tf
+++ main.tf
@@ -1,7 +1,19 @@
 
-resource "aws_eip" "my-eip" {
-  vpc = true
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.web.id
+  allocation_id = aws_eip.example.id
+}
+
+resource "aws_instance" "web" {
+  ami               = "ami-079b5e5b3971bd10d"
+  availability_zone = "us-west-2a"
+  instance_type     = "t2.micro"
+
   tags = {
-    Name = "my-eip"
+    Name = "HelloWorld"
   }
+}
+
+resource "aws_eip" "example" {
+  vpc = true
 }
