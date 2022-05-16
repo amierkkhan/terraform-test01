d174c1cb1f0b03e227bc886fa011f71cf28deaa6
Amir khan <amierkkhan@gmail.com>
2022-05-16 12:03:27 +0530

    01

---
+++ config.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 0.12.5"
+  backend "s3" {
+    bucket = "terraformbackendbucket6321"
+    key    = "test-1/terraform.tfstate"
+    region = "ap-south-1"
+    access_key = "AKIAVDF3YEHUSDBUI6VI"
+    secret_key = "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"
+  }
+   required_providers {
+        aws = {
+            version=">=3.63"
+            source ="hashicorp/aws"
+        }
+    }
+}
+provider "aws" {
+    region = "ap-south-1"
+    access_key = "AKIAVDF3YEHUSDBUI6VI"
+    secret_key = "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"   
+}
